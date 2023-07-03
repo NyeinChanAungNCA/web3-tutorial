ff02bd964335f958fbb0c8ba1b5af9f75c43efbb
Nyein Chan Aung <nyeinchanaung.nk@gmail.com>
2023-07-03 10:14:10 +0630

    Add env variables

--- src/1_accounts.js
+++ src/1_accounts.js
@@ -1,7 +1,11 @@
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "../.env"),
+});
 const { Web3 } = require('web3');
-const rpcURL = 'https://mainnet.infura.io/v3/cf13a29513a44652923c85f254b4ec63' // Your RCkP URL goes here
+const rpcURL = `${process.env.RPC_URL}/${process.env.INFURA_API_KEY}` // Your RCkP URL goes here
 const web3 = new Web3(rpcURL)
-const address = '0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5' // Your account address goes here
+const address = process.env.ACCOUNT_ADDRESS // Your account address goes here
 // web3.eth.getBalance(address, (err, wei) => { balance = web3.utils.fromWei(wei, 'ether') })
 web3.eth.getBalance(address)
     .then((bal) => console.log(web3.utils.fromWei(bal, 'ether')));
