9e15b5b0351767d8231168681aa743e03ed03ea3
NyeinChanAung MacBook Air <nyeinchanaung.nk@gmail.com>
2023-07-05 12:19:55 +0630

    use utilities

---
+++ src/8_web3_extras.js
@@ -0,0 +1,28 @@
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "../.env"),
+});
+const { Web3 } = require('web3');
+const rpcURL = `${process.env.RPC_URL}/${process.env.INFURA_API_KEY}` // Your RCkP URL goes here
+const web3 = new Web3(rpcURL)
+
+// Get average gas price in wei from last few blocks median gas price
+web3.eth.getGasPrice().then((result) => {
+    console.log(web3.utils.fromWei(result, 'ether'))
+})
+
+// Use sha256 Hashing function
+console.log(web3.utils.sha3('Dapp University'))
+
+// Use keccak256 Hashing function (alias)
+console.log(web3.utils.keccak256('Dapp University'))
+
+// Get a Random Hex
+console.log(web3.utils.randomHex(32))
+
+// Get access to the underscore JS library
+// const _ = web3.utils._
+
+// _.each({ key1: 'value1', key2: 'value2' }, (value, key) => {
+//     console.log(key)
+// })
